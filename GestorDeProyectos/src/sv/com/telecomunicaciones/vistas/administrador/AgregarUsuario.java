1f640092390bc674feae8fd0734351f48f023eab
DiegoChevez <diegochevez144@gmail.com>
2023-03-06 17:22:54 -0600

    Ultima Actualizacion

---
+++ GestorDeProyectos/src/sv/com/telecomunicaciones/vistas/administrador/AgregarUsuario.java
@@ -0,0 +1,13 @@
+package sv.com.telecomunicaciones.vistas.administrador;
+
+import javax.swing.*;
+
+public class AgregarUsuario {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField6;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JButton agregarButton;
+}
